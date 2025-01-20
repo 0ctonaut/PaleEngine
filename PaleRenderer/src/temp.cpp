a3d06a2a6934d9f33def86fddfe3834837ec3c36
oct0naut <oct0naut@outlook.com>
2025-01-20 14:36:34 +0800

    add submodule glfw & imgui docking

--- PaleRenderer/src/temp.cpp
+++ PaleRenderer/src/temp.cpp
@@ -3,7 +3,7 @@
 #include <filesystem>
 // -------
 #include <glad/glad.h>
-#include <glfw3.h>
+#include <glfw/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
